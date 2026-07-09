bb4ebe421cf163d4dd876aaa23b342bf21f99bab
ILLUMINATIFreemason <shilafreemason@gmail.com>
2026-07-09 14:27:53 +0430

    Implement showPage function for page navigation
    Add showPage function to manage active page display

--- app.js
+++ app.js
@@ -25,3 +25,20 @@ page.classList.add("active");
 
 
 }
+function showPage(pageId){
+
+console.log("صفحه:", pageId);
+
+document.querySelectorAll(".page")
+.forEach(function(page){
+page.classList.remove("active");
+});
+
+
+let page = document.getElementById(pageId);
+
+if(page){
+page.classList.add("active");
+}
+
+}
